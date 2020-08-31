6d2b99596ed654635350dc3e5932ee5104653fb2
Angelesp0 <angel-ed-s@hotmail.com>
2020-08-31 17:58:16 -0600

    Dashboard

--- app/models/services.model.js
+++ app/models/services.model.js
@@ -1,5 +1,5 @@
 const sql = require("./db.js");
-// constructor
+
 const Services = function(service) {
     this.id_service = service.id_service;
     this.name_service = service.name_service;
@@ -7,7 +7,7 @@ const Services = function(service) {
     this.price = service.price;
     this.img = service.img;
 };
-// Create one company
+
 Services.create1 = (newService, result) => {
     console.log("2.- Model");
     sql.query("INSERT INTO services SET ?", newService, (err, res) => {
@@ -21,7 +21,6 @@ Services.create1 = (newService, result) => {
     });
 };
 
-// Get 1 User From ID
 Services.findById = (id_company, result) => {
     console.log("2.- Model");
     sql.query(`SELECT name_service, desc_service, price, start_date, end_date, status, img FROM company_has_services, services WHERE company_id_company = ${id_company} AND services_id_service = id_service `, (err, res) => {
@@ -30,18 +29,15 @@ Services.findById = (id_company, result) => {
             result(err, null);
             return;
         }
-
         if (res.length) {
             result(null, res[0]);
             return;
         }
-
         // not found Customer with the id
         result({ kind: "La empresa no tiene servicios registrados" }, null);
     });
 };
 
-// Get 1 User From ID
 Services.findServiceById = (id_service, result) => {
     console.log("2.- Model");
     //     sql.query(`SELECT id_company, company, name_service,desc_service,status,start_date,end_date  FROM users, company, company_has_services, services WHERE id_user= ${userId} AND users_id_user=id_user AND id_company=company_id_company AND services_id_service=id_service`, (err, res) => {
@@ -63,8 +59,6 @@ Services.findServiceById = (id_service, result) => {
     });
 };
 
-
-// Get 1 User From ID
 Services.findServiceByUserId = (id_user, result) => {
     console.log("2.- Model");
     sql.query(`SELECT id_company, company, name_service,desc_service,status,start_date,end_date  FROM users, company, company_has_services, services WHERE id_user= ${id_user} AND users_id_user=id_user AND id_company=company_id_company AND services_id_service=id_service`, (err, res) => {
@@ -73,18 +67,15 @@ Services.findServiceByUserId = (id_user, result) => {
             result(err, null);
             return;
         }
-
         if (res.length) {
             result(null, res);
             return;
         }
-
         // not found Customer with the id
         result({ kind: "La empresa no tiene servicios registrados" }, null);
     });
 };
 
-// Get All Users
 Services.getAll = result => {
     console.log("2.- Model");
     sql.query("SELECT * FROM services", (err, res) => {
@@ -97,7 +88,6 @@ Services.getAll = result => {
     });
 };
 
-// update User
 Services.updateById = (id_service, service, result) => {
     sql.query(
         "UPDATE services SET name_service = ?, desc_service = ?, price = ? WHERE id_service = ? ", [service.name_service, service.desc_service, service.price, id_service],
@@ -107,20 +97,17 @@ Services.updateById = (id_service, service, result) => {
                 result(null, err);
                 return;
             }
-
             if (res.affectedRows == 0) {
                 // not found Customer with the id
                 result({ kind: "not_found" }, null);
                 return;
             }
-
             // console.log("updated usuario: ", { id_service: id_service, ...service });
             result(null, { id_service: id_service, ...service });
         }
     );
 };
 
-// remove companies
 Services.remove = (id_service, result) => {
     sql.query("DELETE FROM services WHERE id_service = ?", id_service, (err, res) => {
         if (err) {
@@ -158,12 +145,11 @@ Services.findByCompanyId = (id_company, result) => {
     });
 }
 
-// Create one company
 Services.createRelation = (company_has_Services, result) => {
     console.log("2.- Model");
     sql.query("INSERT INTO company_has_services SET ?", company_has_Services, (err, res) => {
         if (err) {
-            console.log("error1: ", err);
+            console.log("error11: ", err);
             result(err, null);
             return;
         }
@@ -175,22 +161,18 @@ Services.createRelation = (company_has_Services, result) => {
 Services.payment = (id_company, id_service, result) => {
     console.log(id_company);
     console.log(id_service.status);
-
     let date_ob = new Date();
-
     // current year
     let year = date_ob.getFullYear();
     // current month
     let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
     let month1 = ("0" + (date_ob.getMonth() + 2)).slice(-2);
-
     // adjust 0 before single digit date
     let date = ("0" + date_ob.getDate()).slice(-2);
     let datenow = (year + "-" + month + "-" + date);
     let dateend = (year + "-" + month1 + "-" + date);
     console.log(datenow);
     console.log(dateend);
-
     console.log("2.- Model");
     sql.query(
         `UPDATE company_has_services SET status = 1, start_date = '${datenow}', end_date= '${dateend}'  WHERE company_id_company = ${id_company} AND services_id_service = ${id_service.services_id_service} `,
@@ -201,13 +183,11 @@ Services.payment = (id_company, id_service, result) => {
                 result(null, err);
                 return;
             }
-
             if (res.affectedRows == 0) {
                 // not found Customer with the id
                 result({ kind: "not_found" }, null);
                 return;
             }
-
             console.log("updated payment: ", { id_company: id_company });
             result(null, { id_company: id_company });
         }
@@ -225,6 +205,5 @@ Services.register = (payment, result) => {
         result(null, { id_table: res.insertId, ...payment });
     });
 };
-
 
 module.exports = Services;
