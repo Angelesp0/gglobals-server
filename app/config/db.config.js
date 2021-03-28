213a0812efb6f816f435efdce51c7a4af34868c4
Angelesp0 <angel-ed-s@hotmail.com>
2021-03-28 07:26:26 -0700

    hola

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,22 @@
 module.exports = {
-    host: '192.168.100.71',
+    host: 'gglobals.cyrpndhsn9yf.us-east-2.rds.amazonaws.com',
+    user: 'gglobals',
+    password: 'Gestoria123$',
+    database: 'gglobals-app',
+    port: 3306
+};
+/*    host: '192.168.100.71',
     user: 'app',
     password: 'root',
     database: 'gglobals-app',
     port: 3306
-};
+     */
+
+
 
 
 /*module.exports = {
-    host: '201.107.4.46',
+    host: '192.168.100.71',
     user: 'gglobals_app',
     //     user: 'app',
     password: 'Gestoria123$',
